c746a4a33c7cf6d329143bafa7adbda909fe9dca
DaiWangsheng <daiwangsheng@appmobile.local>
2016-09-27 14:29:42 +0800

    Initial Commit

---
+++ block/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  block
+//
+//  Created by DaiWangsheng on 16/9/27.
+//  Copyright © 2016年 DaiWangsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ block/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  block
+//
+//  Created by DaiWangsheng on 16/9/27.
+//  Copyright © 2016年 DaiWangsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
