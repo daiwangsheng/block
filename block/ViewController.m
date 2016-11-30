9a5cb80e22c5e53605b3fc38fd146283fda37d60
daiwangsheng <596817184@qq.com>
2016-11-30 14:34:33 +0800

    Revert "change something"
    This reverts commit 6a4883f8c29a0de868a271684250894bf094fe6e.

--- block/ViewController.m
+++ block/ViewController.m
@@ -7,12 +7,9 @@
 //
 
 #import "ViewController.h"
-#import "SecondViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UITextView *textView;
-@property (nonatomic,copy) NSString * (^stringBlock)(NSString *str);
-@property (nonatomic,copy) NSNumber * (^numberBlock)(NSString *str);
+
 @end
 
 @implementation ViewController
@@ -20,26 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    _stringBlock = ^NSString *(NSString *str){
-        return [str substringWithRange:NSMakeRange(0, 3)];
-    };
-    _numberBlock = ^NSNumber *(NSString *num){
-        return (NSNumber *)num;
-    };
 }
 
-- (IBAction)push:(id)sender {
-    NSNumber *num = self.numberBlock(self.stringBlock(@"12abc"));
-    NSLog(@"%@",num);
-    
-    SecondViewController *vc = [[SecondViewController alloc] init];
-    vc.textBlock = ^(NSString *str){
-        self.textView.text = str;
-    };
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
-    [self presentViewController:nav animated:YES completion:nil];
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
